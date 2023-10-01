5db2149da07e66e9641a32f5fcadb19766bdfa4b
Dmitriy Khomos <dimensenss@gmail.com>
2023-10-01 22:39:59 +0300

    added tag slider and product info

--- sneakers/sneakers_shop/templatetags/sneakers_tags.py
+++ sneakers/sneakers_shop/templatetags/sneakers_tags.py
@@ -13,6 +13,14 @@ def get_menu():
 def get_categories():
     return Category.objects.all()
 
+@register.simple_tag(name='get_photos')
+def get_photos(post):
+    return post.images.all()
+
+@register.simple_tag(name='get_main_photo')
+def get_photos(post):
+    return post.photo.url
+
 @register.inclusion_tag('sneakers_shop/list_categories.html')
 def show_categories(sort = None, cat_selected = None):#доделать
     if sort:
@@ -24,3 +32,7 @@ def show_categories(sort = None, cat_selected = None):#доделать
 @register.inclusion_tag('includes/paginator.html')
 def show_paginator(paginator, page_obj):
     return {"paginator":paginator, 'page_obj':page_obj}
+
+@register.inclusion_tag('includes/slider.html')
+def show_slider(post):
+    return {"post":post}
