0fa58fe2ce21d8a04fc87909f85797bb00045367
Dmytro Khoma <dimensenss@gmail.com>
2024-01-05 20:26:42 +0200

    Настройка цветов и размеров через многие-к-многим

--- sneakers/sneakers_shop/admin.py
+++ sneakers/sneakers_shop/admin.py
@@ -7,10 +7,12 @@ class SneakersImageInline(admin.TabularInline):
     model = ProductImage
     extra = 0
     readonly_fields = ['get_image']
+
     def get_image(self, object):
         return mark_safe(f"<img src='{object.image.url}' width='100' />")
 
     get_image.short_description = 'Зображення'
+
 
 @admin.register(ProductImage)
 class SneakersImages(admin.ModelAdmin):
@@ -25,16 +27,26 @@ class SneakersImages(admin.ModelAdmin):
     get_html_main_photo.short_description = 'Головне фото'
 
 
+class ProductVariantsInline(admin.TabularInline):
+    model = Variants
+    readonly_fields = ('image_tag',)
+    extra = 1
+    show_change_link = True
+
+
 @admin.register(Sneakers)
 class SneakersAdmin(admin.ModelAdmin):
-    list_display = ('id', 'title', 'get_html_main_photo', 'get_html_content', 'get_html_tag_list', 'time_create', 'is_published')
+    list_display = (
+    'id', 'title', 'get_html_main_photo', 'get_html_content', 'get_html_tag_list', 'time_create', 'is_published')
     list_display_links = ('id', 'title')
-    inlines = [SneakersImageInline, ]
+    inlines = [SneakersImageInline, ProductVariantsInline, ]
     search_fields = ('title', 'content')
     list_editable = ('is_published',)
     list_filter = ('is_published', 'time_create')
     prepopulated_fields = {'slug': ('title',)}
-    fields = ('title', 'slug', 'price', 'discount',  'calculate_discount', 'content', 'cat', 'is_published', 'time_create', 'tags', 'get_html_tag_list')
+    fields = (
+    'title', 'slug', 'price', 'discount', 'calculate_discount', 'content', 'cat', 'is_published', 'time_create', 'tags',
+    'get_html_tag_list')
     readonly_fields = ('get_html_main_photo', 'time_create', 'get_html_tag_list', 'calculate_discount')
 
     def get_html_content(self, object):
@@ -52,7 +64,7 @@ class SneakersAdmin(admin.ModelAdmin):
     def calculate_discount(self, object):
         s = f"Актуальна ціна: {object.sell_price} \n "
 
-        discount = (object.price - object.discount)*100/object.price
+        discount = (object.price - object.discount) * 100 / object.price
 
         if discount < 100:
             s += f"Знижка: {discount} %"
@@ -63,6 +75,14 @@ class SneakersAdmin(admin.ModelAdmin):
     get_html_tag_list.short_description = 'Теги'
     calculate_discount.short_description = 'Актуальна ціна'
 
+@admin.register(Color)
+class ColorAdmin(admin.ModelAdmin):
+    list_display = ['name', 'code', 'color_tag']
+
+@admin.register(Size)
+class SizeAdmin(admin.ModelAdmin):
+    list_display = ['name', 'code']
+
 
 @admin.register(Category)
 class CategoryAdmin(admin.ModelAdmin):
@@ -70,6 +90,3 @@ class CategoryAdmin(admin.ModelAdmin):
     list_display_links = ('id', 'name')
     search_fields = ('name',)
     prepopulated_fields = {'slug': ('name',)}
-
-
-
--- sneakers/sneakers_shop/models.py
+++ sneakers/sneakers_shop/models.py
@@ -3,10 +3,19 @@ from django.urls import reverse, reverse_lazy
 
 
 # Create your models here.
+from django.utils.safestring import mark_safe
 from taggit.managers import TaggableManager
 
 
 class Sneakers(models.Model):
+    VARIANTS = (
+        ('None', 'None'),
+        ('Size', 'Size'),
+        ('Color', 'Color'),
+        ('Size-Color', 'Size-Color'),
+
+    )
+
     title = models.CharField(max_length=255, verbose_name='Назва')
     slug = models.SlugField(max_length=255, unique=True, db_index=True, verbose_name= "URL")
     content = models.TextField(blank=True, verbose_name='Контент')
@@ -18,6 +27,8 @@ class Sneakers(models.Model):
     cat = models.ForeignKey('Category', on_delete=models.PROTECT, null=True, verbose_name="Категорія")
     tags = TaggableManager()
     sell_price = models.DecimalField(default=0.0, max_digits=7, decimal_places=2, verbose_name='Актуальна ціна')
+    variant = models.CharField(max_length=10, choices=VARIANTS, default='None')
+
     def save(self, *args, **kwargs):
         self.sell_price = self.calculate_sell_price()
         super().save(*args, **kwargs)
@@ -38,6 +49,55 @@ class Sneakers(models.Model):
         verbose_name = 'Кросівки'
         verbose_name_plural = 'Кросівки'
         ordering = ['-time_create', 'title']
+
+class Color(models.Model):
+    name = models.CharField(max_length=20)
+    code = models.CharField(max_length=10, blank=True, null=True)
+    def __str__(self):
+        return self.name
+    def color_tag(self):
+        if self.code is not None:
+            return mark_safe('<p style="background-color:{}">Color </p>'.format(self.code))
+        else:
+            return ""
+
+
+class Size(models.Model):
+    name = models.CharField(max_length=20)
+    code = models.CharField(max_length=10, blank=True,null=True)
+    def __str__(self):
+        return self.name
+
+class Variants(models.Model):
+    title = models.CharField(max_length=100, blank=True,null=True)
+    sneakers = models.ForeignKey(Sneakers, on_delete=models.CASCADE)
+    color = models.ForeignKey(Color, on_delete=models.CASCADE,blank=True,null=True)
+    size = models.ForeignKey(Size, on_delete=models.CASCADE,blank=True,null=True)
+    image_id = models.IntegerField(blank=True,null=True,default=0)
+    quantity = models.IntegerField(default=1)
+    price = models.DecimalField(max_digits=12, decimal_places=2,default=0)
+
+    def __str__(self):
+        return self.title
+
+    def image(self):
+        img = ProductImage.objects.get(id=self.image_id)
+        if img.id is not None:
+             varimage=img.image.url
+        else:
+            varimage=""
+        return varimage
+
+    def image_tag(self):
+        img = ProductImage.objects.get(id=self.image_id)
+        if img.id is not None:
+             return mark_safe('<img src="{}" height="50"/>'.format(img.image.url))
+        else:
+            return ""
+
+
+
+
 
 class Category(models.Model):
     name = models.CharField(max_length=100, db_index=True, verbose_name="Имя")
