6612c804dfd96f6576dd0dc53763b323c5ba5357
Dmytro Khoma <dimensenss@gmail.com>
2024-01-07 15:16:44 +0200

    форма регистрации

--- sneakers/users/forms.py
+++ sneakers/users/forms.py
@@ -1,14 +1,14 @@
 from django import forms
 from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
-from django.contrib.auth.models import User
+from users.models import User
 
 
 
 class RegisterUserForm(UserCreationForm):
-    username = forms.CharField(label='Username', widget=forms.TextInput(attrs={'class':"form-control"}))
-    email = forms.EmailField(label='Email',widget=forms.EmailInput(attrs={'class':"form-control"}))
-    password1 = forms.CharField(label='Password', widget=forms.PasswordInput(attrs={'class':"form-control"}))
-    password2 = forms.CharField(label='Retry password', widget=forms.PasswordInput(attrs={'class':"form-control"}))
+    username = forms.CharField()
+    email = forms.EmailField()
+    password1 = forms.CharField()
+    password2 = forms.CharField()
     class Meta:
         model = User
         fields = ('username', 'email', 'password1', 'password2')
