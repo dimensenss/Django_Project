b7574c29eb125ea185437931a2cdad56fae4ecd6
Dmytro Khoma <dimensenss@gmail.com>
2024-01-10 21:38:55 +0200

    изменение количества товара в корзине через ajax

--- sneakers/static/sneakers_shop/js-scripts/scripts.js
+++ sneakers/static/sneakers_shop/js-scripts/scripts.js
@@ -1,24 +1,23 @@
-
-$(document).ready(function($) {
+$(document).ready(function ($) {
     var successMessage = $("#jq-notification");
-	var notification = $('#notification');
+    var notification = $('#notification');
 
-	if (notification.length > 0){
-		setTimeout(function (){
-			notification.alert('close');
+    if (notification.length > 0) {
+        setTimeout(function () {
+            notification.alert('close');
 
-		},1500);
-	}
+        }, 1500);
+    }
 
 
-	// Ловим собыитие клика по кнопке добавить в корзину
+    // Ловим собыитие клика по кнопке добавить в корзину
     $(document).on("click", ".add-to-cart", function (e) {
         // Блокируем его базовое действие
         e.preventDefault();
 
         // Берем элемент счетчика в значке корзины и берем оттуда значение
-       var goodsInCartCount = $(".goods-in-cart-count");
-       var cartCount = parseInt(goodsInCartCount.first().text() || 0); // Выбираем первый элемент
+        var goodsInCartCount = $(".goods-in-cart-count");
+        var cartCount = parseInt(goodsInCartCount.first().text() || 0); // Выбираем первый элемент
 
         // Получаем id товара из атрибута data-product-id
         var product_id = $(this).data("product-id");
@@ -60,16 +59,14 @@ $(document).ready(function($) {
     });
 
 
-
-
-	// Ловим собыитие клика по кнопке удалить товар из корзины
+    // Ловим собыитие клика по кнопке удалить товар из корзины
     $(document).on("click", ".remove-from-cart", function (e) {
         // Блокируем его базовое действие
         e.preventDefault();
 
         // Берем элемент счетчика в значке корзины и берем оттуда значение
-       var goodsInCartCount = $(".goods-in-cart-count");
-       var cartCount = parseInt(goodsInCartCount.first().text() || 0); // Выбираем первый элемент
+        var goodsInCartCount = $(".goods-in-cart-count");
+        var cartCount = parseInt(goodsInCartCount.first().text() || 0); // Выбираем первый элемент
 
         // Получаем id корзины из атрибута data-cart-id
         var cart_id = $(this).data("cart-id");
@@ -111,42 +108,113 @@ $(document).ready(function($) {
     });
 
 
+    // Теперь + - количества товара
+    // Обработчик события для уменьшения значения
+    var isUpdatingCart = false; // Флаг, который указывает, идет ли в данный момент процесс обновления корзины
+
+    $(document).on("click", ".decrement", function () {
+        if (!isUpdatingCart) { // Проверяем, не идет ли уже процесс обновления корзины
+            isUpdatingCart = true; // Устанавливаем флаг, что процесс начался
+
+            var url = $(this).data("cart-change-url");
+            var cartID = $(this).data("cart-id");
+            var $input = $(this).closest('.input-group').find('.number');
+            var currentValue = parseInt($input.val());
+
+            if (currentValue > 1) {
+                $input.val(currentValue - 1);
+                updateCart(cartID, currentValue - 1, -1, url);
+
+                // Устанавливаем задержку в 1 секунду (или любое другое значение в миллисекундах)
+                setTimeout(function () {
+                    isUpdatingCart = false; // По истечении времени задержки снимаем флаг
+                }, 200);
+            } else {
+                isUpdatingCart = false; // Если количество уже 1, снимаем флаг без задержки
+            }
+        }
+    });
+
+    $(document).on("click", ".increment", function () {
+        if (!isUpdatingCart) {
+            isUpdatingCart = true;
+
+            var url = $(this).data("cart-change-url");
+            var cartID = $(this).data("cart-id");
+            var $input = $(this).closest('.input-group').find('.number');
+            var currentValue = parseInt($input.val());
+
+            $input.val(currentValue + 1);
+            updateCart(cartID, currentValue + 1, 1, url);
+
+            setTimeout(function () {
+                isUpdatingCart = false;
+            }, 200);
+        }
+    });
+
+    function updateCart(cartID, quantity, change, url) {
+        $.ajax({
+            type: "POST",
+            url: url,
+            data: {
+                cart_id: cartID,
+                quantity: quantity,
+                csrfmiddlewaretoken: $("[name=csrfmiddlewaretoken]").val(),
+            },
+
+            success: function (data) {
+                // Сообщение
+
+                // Изменяем количество товаров в корзине
+                var goodsInCartCount = $(".goods-in-cart-count");
+                var cartCount = parseInt(goodsInCartCount.first().text() || 0); // Выбираем первый элемент
+                cartCount += change;
+                goodsInCartCount.text(cartCount);
+
+                // Меняем содержимое корзины
+                var cartItemsContainer = $("#cart-items-container");
+                cartItemsContainer.html(data.cart_items_html);
+
+            },
+            error: function (data) {
+                console.log("Ошибка при добавлении товара в корзину");
+            },
+        });
+    }
 
 
+    $('.image-popup-vertical-fit').magnificPopup({
+        type: 'image',
+        closeOnContentClick: true,
+        mainClass: 'mfp-img-mobile',
+        image: {
+            verticalFit: true
+        }
 
+    });
 
+    $('.image-popup-fit-width').magnificPopup({
+        type: 'image',
+        closeOnContentClick: true,
+        image: {
+            verticalFit: false
+        }
+    });
 
-	$('.image-popup-vertical-fit').magnificPopup({
-		type: 'image',
-		closeOnContentClick: true,
-		mainClass: 'mfp-img-mobile',
-		image: {
-			verticalFit: true
-		}
-
-	});
-
-	$('.image-popup-fit-width').magnificPopup({
-		type: 'image',
-		closeOnContentClick: true,
-		image: {
-			verticalFit: false
-		}
-	});
-
-	$('.image-popup-no-margins').magnificPopup({
-		type: 'image',
-		closeOnContentClick: true,
-		closeBtnInside: false,
-		fixedContentPos: true,
-		mainClass: 'mfp-no-margins mfp-with-zoom', // class to remove default margin from left and right side
-		image: {
-			verticalFit: true
-		},
-		zoom: {
-			enabled: true,
-			duration: 300 // don't foget to change the duration also in CSS
-		}
-	});
+    $('.image-popup-no-margins').magnificPopup({
+        type: 'image',
+        closeOnContentClick: true,
+        closeBtnInside: false,
+        fixedContentPos: true,
+        mainClass: 'mfp-no-margins mfp-with-zoom', // class to remove default margin from left and right side
+        image: {
+            verticalFit: true
+        },
+        zoom: {
+            enabled: true,
+            duration: 300 // don't foget to change the duration also in CSS
+        }
+    });
 
 });
